2f4eeb2670c2a46c4b84d9f75b586208c5162374
rika <rikayolanda@student.uir.ac.id>
2024-04-26 22:06:02 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  Tujuan APK ini dibuat,agar kamu lebih teratur dalam menjalani harimu sehingga membuat hidupmu lebih terstruktur dan disiplin!
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
